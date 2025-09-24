53c33b6c7c3d8f305d3d6fd3a8f9da350464aa97
PasupathiG03 <pasupathi0322@gmail.com>
2025-09-24 14:03:02 +0530

    bookapp push

--- library/views.py
+++ library/views.py
@@ -1,13 +1,29 @@
-from django.shortcuts import render, redirect, get_list_or_404
+from django.shortcuts import render, redirect
 from .models import Book
-# from .forms import BookForm
-
+from .forms import BookForm
 
 def book_list(request):
     books = Book.objects.all()
-    return render(request, 'library/book_list.html', {'books':books})
+    return render(request, 'library/book_list.html', {'books': books})
 
 def book_create(request):
-    
+    form = BookForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('book_list')
     return render(request, 'library/book_form.html', {'form': form})
 
+def book_update(request, pk):
+    book = Book.objects.get(id=pk)
+    form = BookForm(request.POST or None, instance=book)
+    if form.is_valid():
+        form.save()
+        return redirect('book_list')
+    return render(request, 'library/book_form.html', {'form': form})
+
+def book_delete(request, pk):
+    book = Book.objects.get(id=pk)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book_list')
+    return render(request, 'library/book_confirm_delete.html', {'book': book})
